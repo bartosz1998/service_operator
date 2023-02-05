2b90fa941e0594f770b2fbfa3f367447eae0669c
bartosz <bartoszbizon1998@gmail.com>
2023-02-05 13:00:51 +0100

    trying create Navigation Drawer

--- app/src/main/java/com/example/service_operator/MainActivity.kt
+++ app/src/main/java/com/example/service_operator/MainActivity.kt
@@ -2,30 +2,56 @@ package com.example.service_operator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
-import androidx.fragment.app.Fragment
 import com.example.service_operator.fragment.LargeNewsFragment
 import com.example.service_operator.fragment.LoginActivityFragment
-import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
 
-//        replaceFragment(LargeNewsFragment())
-        supportFragmentManager.beginTransaction().replace(R.id.frame_layout, LoginActivityFragment()).commit()
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, LoginActivityFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
+        }
+
+
 
     }
 
-//    private fun replaceFragment(largeNewsFragment: Fragment) {
-//
-//        val fragmentManager = supportFragmentManager
-//        val fragmentTransaction = fragmentManager.beginTransaction()
-//        fragmentTransaction.replace(R.id.frame_layout, largeNewsFragment)
-//        fragmentTransaction.commit()
-//
-//    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.frame_layout, LargeNewsFragment()).commit()
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
 }
